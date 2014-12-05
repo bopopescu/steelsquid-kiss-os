2a459ac076d6edbaf19ee986b6f60da1822965ed
Steelsquid <steelsquid@steelsquid-worker.(none)>
2014-12-05 15:02:23 +0100

    -

--- steelsquid-kiss-os.sh
+++ steelsquid-kiss-os.sh
@@ -3704,8 +3704,8 @@ log "Change from bash to dash done"
 # Generate locale
 ##################################################################################
 log "Generate locale"
-echo "LANG=en_GB.UTF-8" > /etc/default/locale
-echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/default/locale
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 log "Generate locale"
 
