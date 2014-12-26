1f70ad1c826bacffce95dad278bab82d8e9497ef
Steelsquid <steelsquid@steelsquid-worker.(none)>
2014-12-26 20:13:18 +0100

    -

---
+++ tester.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python -OO
+
+import steelsquid_utils
+import steelsquid_pi
+import time
+
+#def cm(address, pin, value):
+#    steelsquid_utils.shout(value)
+
+#steelsquid_pi.ads1015_event(49, 0, cm)
+
+
