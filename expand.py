9454e1b2386bf8c32aa279a648f5cc80272e6b4a
Steelsquid <steelsquid@steelsquid-worker.(none)>
2014-12-26 20:24:40 +0100

    -

--- expand.py
+++ expand.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python -OO
 
+'''
+Use this to implemet or test your own stuff.
+Will execute in own thread.
+
+@organization: Steelsquid
+@author: Andreas Nilsson
+@contact: steelsquid@gmail.com
+@license: GNU Lesser General Public License v2.1
+@change: 2014-12-26 Created
+'''
+
 import steelsquid_utils
 import steelsquid_pi
-import time
 
-#def cm(address, pin, value):
-#    steelsquid_utils.shout(value)
-
-#steelsquid_pi.ads1015_event(49, 0, cm)
 
 
