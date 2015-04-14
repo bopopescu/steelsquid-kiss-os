a72cbf20bb6e21f3bcd540669b824e722c8e1302
Steelsquid <steelsquid@steelsquid-worker.(none)>
2015-04-14 22:33:49 +0200

    -

--- steelsquid-kiss-os.sh
+++ steelsquid-kiss-os.sh
@@ -1212,7 +1212,7 @@ if [ "$in_parameter_1" == "help-utils" ]; then
 	exit 0
 fi
 ##################################
-function help_build()
+function help_build()Kontonummer
 {
     echo 
     echo 
@@ -1222,11 +1222,11 @@ function help_build()
     echb "############################################################################"
     echo 
     echb "1.  Download and extract the installer image"
-    echo "wget https://github.com/debian-pi/raspbian-ua-netinst/releases/download/v1.0.5/raspbian-ua-netinst-v1.0.5.img.xz"
-    echo "unxz raspbian-ua-netinst-v1.0.5.img.xz"
+    echo "wget https://github.com/debian-pi/raspbian-ua-netinst/releases/download/v1.0.6/raspbian-ua-netinst-v1.0.6.img.xz"
+    echo "unxz raspbian-ua-netinst-v1.0.6.img.xz"
     echo 
     echb "2.  Copy to sdcard"
-    echo "dd bs=4M if=raspbian-ua-netinst-v1.0.5.img of=/dev/sdb"
+    echo "dd bs=4M if=raspbian-ua-netinst-v1.0.6.img of=/dev/sdb"
     echo 
     echb "3.  Insert sdcard into raspberry and boot"
     echo "Must have the networkcabel connected (internet access)"
@@ -1240,15 +1240,17 @@ function help_build()
     echb "5.  Set password"
     echo "passwd  (raspberry)"
     echo 
-    echb "6.  Change to testing and uppgrade"
+    echb "7.  Change to testing and uppgrade"
     echo "echo \"deb http://mirrordirector.raspbian.org/raspbian testing main firmware contrib non-free rpi\" > /etc/apt/sources.list"
+    echo "echo \"deb http://archive.raspberrypi.org/debian wheezy main untested\" >> /etc/apt/sources.list"
     echo "apt-get update"
     echo "apt-get --no-install-recommends install sudo nano aptitude    "
+    echo "aptitude update"
     echo "aptitude full-upgrade"
     echo "dpkg --configure -a"
     echo "cp /vmlinuz /boot/kernel.img"
     echo 
-    echb "7.  Clean"
+    echb "8.  Clean"
     echo "aptitude autoclean"
     echo "aptitude clean"
     echo "apt-get clean"
@@ -1256,45 +1258,45 @@ function help_build()
     echo "rm /root/.bash_history"
     echo "rm /root/.nano_history"
     echo 
-    echb "8.  Shutdown and mount on computer"
+    echb "9.  Shutdown and mount on computer"
     echo 
-    echb "9.  Resize to 3.4G with gparted"
+    echb "10.  Resize to 3.4G with gparted"
     echo 
-    echb "10. Boot the raspberry pi again."
+    echb "11. Boot the raspberry pi again."
     echo 
-    echb "11. Download script"
+    echb "12. Download script"
     echo "wget --no-check-certificate http://www.steelsquid.org/steelsquid-kiss-os.sh"
     echo 
-    echb "12. Make executable"
+    echb "13. Make executable"
     echo "chmod +x steelsquid-kiss-os.sh"
     echo 
-    echb "13. Execute the stcript"
+    echb "14. Execute the stcript"
     echo "./steelsquid-kiss-os.sh upgrade"
     echo 
-    echb "14. Take a nap :-)"
+    echb "15. Take a nap :-)"
     echo "May have to answer some questions"
     echo 
-    echb "15. Remove script"
+    echb "16. Remove script"
     echo "rm steelsquid-kiss-os.sh"
     echo 
-    echb "16. Shutdown raspberry"
+    echb "17. Shutdown raspberry"
     echo 
-    echb "17. Insert sdcard in other computer and mount"
+    echb "18. Insert sdcard in other computer and mount"
     echo "rm -R var/log/*"
     echo "rm -R var/tmp/*"
     echo "rm -R tmp/*"
     echo 
-    echb "18. Make a img of it"
+    echb "19. Make a img of it"
     echo "./steelsquid-kiss-os.sh read /dev/sdb"
     echo 
-    echb "19. Compress image to gz"
+    echb "20. Compress image to gz"
     echo "./steelsquid-kiss-os.sh compress"
     echo 
-    echb "20. On new image remember to:"
+    echb "21. On new image remember to:"
     echo "Upload steelsquid-kiss-os.sh to http://www.steelsquid.org"
     echo "Clear the ssh keys"
     echo 
-    echb "21. GIT and github"
+    echb "22. GIT and github"
     echo "git init"
     echo "git add *.html"
     echo "git commit -m \"Initial commit\""
