d745d27d1bb61b94306f82647fb93dfe32fb6e6d
Steelsquid <steelsquid@steelsquid-worker.(none)>
2015-06-05 19:13:39 +0200

    -

---
+++ custom.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python -OO
+
+
+'''.
+This is an example file how to implement your python file
+
+
+If you use steelsquid_syncronisation:
+Add this to config.txt and it will bli imported (executed)
+custom.py | /opt/steelsquid/python/expand/custom.py
+
+If you just want to import in manualy from your code add this to config.txt
+custom.py | /opt/steelsquid/python/custom.py
+
+
+@organization: Steelsquid
+@author: Andreas Nilsson
+@contact: steelsquid@gmail.com
+@license: GNU Lesser General Public License v2.1
+@change: 2014-12-26 Created
+'''
+
+
+import steelsquid_utils
+import steelsquid_event
+import steelsquid_pi
+
---
+++ steelsquid_kiss_expand.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python -OO
+
+
+'''
+Implement your own stuff.
+Will be imported (executed) in its own thread on boot
+
+@organization: Steelsquid
+@author: Andreas Nilsson
+@contact: steelsquid@gmail.com
+@license: GNU Lesser General Public License v2.1
+@change: 2013-10-25 Created
+'''
+
+
+import steelsquid_utils
+import steelsquid_event
+import steelsquid_pi
+
