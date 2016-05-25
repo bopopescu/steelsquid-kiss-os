f78feddec43cb383f70a2f867f7d832bef96b51a
Andreas Nilsson <steelsquid@gmail.com>
2016-05-25 21:20:25 +0200

    -

--- test.py
+++ test.py
@@ -13,9 +13,3 @@ import steelsquid_kiss_global
 import steelsquid_nrf24
 
 
-ser = serial.Serial('/dev/ttyAMA0', parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=8, writeTimeout=0, dsrdtr=True)
-ser.write('S0\r')
-#print ser.readline()
-
-#steelsquid_pi.gpio_setup_in(2, steelsquid_pi.PULL_UP)
-#steelsquid_pi.gpio_setup_in(3, steelsquid_pi.PULL_UP)
